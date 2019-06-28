a59ee700d5ca835d2126887347cf5b4347791ca4
Lee Gainer <piealacode@gmail.com>
2019-06-28 12:41:37 -0400

    Added tabs

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,55 @@
+<script>
+export default {
+  data: {
+    text: "testing"
+  }
+  
+}
+</script>
+
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-tabs
+    centered
+    color="cyan"
+    dark
+    icons-and-text
+  >
+    <v-tabs-slider color="yellow"></v-tabs-slider>
+
+    <v-tab href="#tab-1">
+      All Players
+      <v-icon>people</v-icon>
+    </v-tab>
+
+    <v-tab href="#tab-2">
+      Filter Players
+      <v-icon>filter_none</v-icon>
+    </v-tab>
+
+    <v-tab href="#tab-3">
+      Add Player
+      <v-icon>person_add</v-icon>
+    </v-tab>
+
+    <v-tab-item
+      v-for="i in 3"
+      :key="i"
+      :value="'tab-' + i"
+    >
+      <v-card flat>
+        <v-card-text>{{ text }}</v-card-text>
+      </v-card>
+    </v-tab-item>
+  </v-tabs>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Roboto", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
+
